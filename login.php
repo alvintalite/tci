fc40beb468be83dc01a63de5efa42ebceab92ce9
alvintalite <alvintalite13@gmail.com>
2017-02-28 22:55:32 +0800

    first commit

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tumandok Craft Industries</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom_style.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body class="login">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6">
+				<div class="col-lg-offset-1 col-lg-10">
+					<div class="panel panel-info centered">
+                		<div class="text-center">
+                			<img src="img/tci-logo.png"/>
+                		</div>
+                	<form>
+                		<label class="control-label">E-mail Address:</label>
+                		<div class="form-group input-group">
+                			<span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                			<input class="form-control" type="text" required="" placeholder="Email Address"/>
+                		</div>
+
+                		<label class="control-label">Password:</label>
+                		<div class="form-group input-group">
+                			<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                			<input class="form-control" type="password" required="" placeholder="Password"></input>
+                		</div>
+                		<div class="text-center submit">
+                			<button class="btn btn-success" type="submit">Login</button>
+                		</div>
+                	</form>
+                </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
