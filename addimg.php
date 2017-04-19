ad49c5bd47835bca12bbb5b5d6e107524a3dd61c
icable24 <icable130@gmail.com>
2017-04-19 22:40:38 +0800

    ok

---
+++ addimg.php
@@ -0,0 +1,13 @@
+<?php 
+	require "database.php";
+
+	$pdo = Database::connect();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="post" enctype="multipart/form-data" action="test.php">
+	
+	</form>
+</body>
+</html>
